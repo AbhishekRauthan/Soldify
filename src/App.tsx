e0e341431116fb44741c2f8966c4957abd063b23
abhishek <abhishek.rauthan.10@gmail.com>
2021-08-16 22:59:52 +0530

    added redux and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
+    <div className="wrapper">
+      <Navbar />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
