7c647f45f5d901d2706183dea03ff2c292f624d8
abhishek <abhishek.rauthan.10@gmail.com>
2021-08-19 02:01:10 +0530

    added leixo custom font

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,15 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <header className="sticky top-0 bg-white">
-      <nav className=""></nav>
+    <header className="sticky top-0 bg-white shadow-xl py-4">
+      <div className="container mx-auto px-3 lg:px-0 flex justify-between items-center">
+        <div className="bg-purple-500 skew-y-3">
+          <h1 className="font-lexio text-xl md:text-3xl uppercase -skew-y-3 font-bold ">
+            Soldify
+          </h1>
+        </div>
+        <nav className=""></nav>
+      </div>
     </header>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 module.exports = {
   mode: 'jit',
-  purge: [
-    "./index.html",
-    "./src/**/*.{ts|tsx}"
-  ],
+  purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'lexio': ["LEIXO DEMO"]
+      }
+    },
   },
   variants: {
     extend: {},
