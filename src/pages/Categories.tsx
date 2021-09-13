bdc23059bc3d547d47eb11fd4fe7f8b903626fbd
abhishek <abhishek.rauthan.10@gmail.com>
2021-09-13 13:29:17 +0530

    added Categories to pages folder

---
+++ src/pages/Categories.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+
+const Categories = () => {
+  return (
+    <section className="pt-20">
+      <h1 className="uppercase text-2xl font-lexio text-center text-black p-10">
+        categories page
+      </h1>
+    </section>
+  );
+};
+
+export default Categories;
