f082570b475250f8e5550bfa5de0704549740ce5
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2021-10-04 16:53:13 +0530

    Made UI changes in Error.tsx
    Following were the changes done
      - removed border from h1 heading
      - made span element upright on medium+ screens
      - added gradient to span element

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Error = () => {
   return (
-    <div className="bg-gray-200 grid place-content-center h-[90vh]">
+    <section className="bg-gray-200 grid place-content-center h-[90vh]">
       <div className="container bg-white p-2 flex flex-col md:flex-row md:flex-wrap md:justify-center md:items-center w-2/3 md:w-[90%]  text-center">
-        <h1 className="text-3xl font-light font-lexio border-b-2 border-gray-500 pb-2 md:border-none md:text-7xl">
+        <h1 className="text-3xl font-light font-lexio pb-2 md:text-7xl">
           404 Error
         </h1>
-        <span className="md:h-12 md:w-0.5 md:mx-5 md:bg-gray-800"></span>
+        <span className="h-0.5 w-20 mx-auto md:h-16 md:w-0.5 rounded-full md:mx-5 bg-gradient-to-r from-pink-500 to-purple-500"></span>
         <h3 className="capitalize pt-2 text-2xl md:text-4xl font-proza font-extralight">
           Page not Found
         </h3>
@@ -15,7 +15,7 @@ const Error = () => {
           Either something went wrong or the Page you requested doesn't exists
         </p>
       </div>
-    </div>
+    </section>
   );
 };
 
