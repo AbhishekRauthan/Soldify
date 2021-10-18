bb4998140e43a2d208f273b343c53daf7e360657
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2021-10-18 22:38:34 +0530

    Made the following changes in components/Heading.tsx:
      - added Props interface that provides additional styles
      - extended Props to provide rest of the html heading props

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,8 +1,15 @@
-import React, { FC } from "react";
+import React, { FC, HTMLAttributes } from "react";
 
-const Heading: FC = ({ children }) => {
+interface Props extends HTMLAttributes<HTMLHeadingElement> {
+  additionalStyles?: string;
+}
+
+const Heading: FC<Props> = ({ children, additionalStyles, ...rest }) => {
   return (
-    <h1 className="font-bold tracking-widest font-lexio underline text-lg md:text-xl lg:text-2xl uppercase text-center mb-5 md:mb-7 lg:mb-10">
+    <h1
+      className={`font-bold tracking-widest font-lexio underline text-lg md:text-xl lg:text-2xl uppercase text-center ${additionalStyles}`}
+      {...rest}
+    >
       {children}
     </h1>
   );
