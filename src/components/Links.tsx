b785826770bef6733f22be106e489ce81d16d2c0
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2021-12-14 23:18:47 +0530

    The following changes were done:
      - added a Link component that is restyled
      - utilized Link component in Links component

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,22 +1,31 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { FC } from "react";
+import { Link as RouterLink } from "react-router-dom";
+
+interface Props {
+  href: string;
+}
+
+const Link: FC<Props> = ({ href, children }) => {
+  return (
+    <RouterLink to={`/${href}`}>
+      <div className="">
+        <li className="font-proza list-none text-xl uppercase  peer">
+          {children}
+        </li>
+        <span className="lg:peer-hover:w-full block w-full lg:w-0 h-0.5 rounded-full bg-gradient-to-r from-pink-500 to-purple-500 lg:delay-150 lg:duration-300" />
+      </div>
+    </RouterLink>
+  );
+};
 
 const Links = () => {
   return (
     <>
-      <Link className="nav-div" to="/store">
-        <div className="nav-item peer">Store</div>
-        <span className="bottom-span" />
-      </Link>
-      <Link className="nav-div" to="/categories">
-        <div className="nav-item peer">categories</div>
-        <span className="bottom-span" />
-      </Link>
-      <Link className="nav-div" to="/brands">
-        <div className="nav-item peer">brands</div>
-        <span className="bottom-span" />
-      </Link>
+      <Link href="store">store</Link>
+      <Link href="brands">brands</Link>
+      <Link href="categories">categories</Link>
     </>
   );
 };
+
 export default Links;
